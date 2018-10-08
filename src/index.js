e54e2d04cc291abc25b33a98e8f943720bc472cf
Christopher_pham@yahoo.com <christopher_pham@yahoo.com>
2018-10-07 22:58:05 -0500

    SAve from microsoft

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom'; 
 
-const element = <h1>Hello ld</h1>;
+const element = <h1>Helo ld</h1>;
 
 ReactDOM.render(element, document.getElementById('root'));
