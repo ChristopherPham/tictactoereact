d3e9681d9da46fc78813d775883f1833934c98e5
Chris <chris@falkon.net>
2018-10-09 21:49:19 -0500

    added click handler on redering square

--- src/index.js
+++ src/index.js
@@ -32,7 +32,9 @@ class Square extends React.Component {
           };
       }
     renderSquare(i) {
-      return <Square value= {i} />;
+      return <Square value= {this.state.squares[i]} 
+      onClick={() => this.handleClick(i)}
+      />;
     }
   
     render() {
